c06c4d2400b85e8a42ee34322da6e906d5e0e6f1
ctsfo19 <mmatisolt@gmail.com>
2020-03-06 22:45:35 +0100

    Changes to be committed: 	modified:   .gitignore 	new file:   one.py

---
+++ one.py
@@ -0,0 +1,23 @@
+import random
+from array import *
+
+n = int(input())
+dataFile = open("dane.txt","w")
+countFile = open("ile.txt","w")
+#randTab = array('i',[0])
+#for i in range(0,10):
+#  randTab.append(0)
+countTab = array('i', [0])
+for i in range(0,n):
+  countTab.append(0)
+
+for i in range(0,n):
+  randNum = random.randint(1,10)
+#  if randTab[randNum] == 0:
+  dataFile.write(str(randNum)+"\n")
+#  randTab[randNum] += 1
+  countTab[randNum] += 1
+for i in range(0,11):
+  countFile.write(str(i) + ":" + str(countTab[i]) + "\n")
+dataFile.close()
+countFile.close()
